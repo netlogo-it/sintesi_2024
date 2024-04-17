44e4253f66890320436e71199ebc0b9052adbf8d
netlogo-it <webmaster@netlogo.net>
2024-04-17 13:24:34 +0200

    presentazione webapplications

--- maven/profili/src/main/java/com/example/Main.java
+++ maven/profili/src/main/java/com/example/Main.java
@@ -1,7 +1,24 @@
 package com.example;
 
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Avvio applicazione");
+
+        Properties properties=new Properties();
+        InputStream is=Main.class.getResourceAsStream("/config.properties");
+        try{
+            properties.load(is);
+        }catch(Exception e){
+            System.out.println("Errore nella lettura del file: config.properties");
+        }
+        Enumeration em=properties.keys();
+        while(em.hasMoreElements()){
+            String key=(String) em.nextElement();
+            System.out.println("Property: "+key+"="+properties.getProperty(key));
+        }
     }
 }
